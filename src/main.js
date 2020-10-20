17bcb9285011071b36df8d1e141a3b0e0715945c
demigods <a13752365641@126.com>
2020-10-20 14:40:47 +0800

    添加ant组件。

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Antd from 'ant-design-vue'
+
+import App from './App'
 import router from './router'
 import store from './store'
 
+import 'ant-design-vue/dist/antd.css'
+
+// 引入工具包
+import tools from '@/tools'
+
 Vue.config.productionTip = false
 
+Vue.use(Antd)
+
+// 绑定工具方法
+Vue.prototype.$tools = tools
+
+/* eslint-disable no-new */
 new Vue({
+  components: { App },
   router,
   store,
   render: h => h(App)
